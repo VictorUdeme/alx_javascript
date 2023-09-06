76c30384ab89f4a990f1ab41bc24f9c15382aa9b
Snazzydo <snazzydo9@gmail.com>
2023-09-06 14:37:10 +0100

    Task 6

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
-function CallMeMoby(x, theFunction) {
+function callMeMoby(x, theFunction) {
   for (let i = 0; i < x; i++) {
       theFunction();
   }
 }
 
-module.exports = CallMeMoby;
+module.exports = callMeMobby;
