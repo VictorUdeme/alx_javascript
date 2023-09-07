09f2c649f6d9edf0b01114b1412b5ae124ee288a
Snazzydo <snazzydo9@gmail.com>
2023-09-07 21:01:39 +0100

    Task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const Square1 = require('/.5-square.js');
+const Square1 = require('./5-square');
 class Square extends Square1 {
     charPrint(c) {
       if (c === undefined) {
