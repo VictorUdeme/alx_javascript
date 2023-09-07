0f8c39983715b7e131d72fd2fa7c890d19962edb
Snazzydo <snazzydo9@gmail.com>
2023-09-07 20:32:17 +0100

    Task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,15 +2,15 @@
 
 class Rectangle {
     constructor(w, h) {
-        if (w <= 0 || h <= 0) {
-            this.width = 0;
-            this.height = 0;
-        } else {
-            this.width = w;
-            this.height = h;
-        }
-        
+      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+        // If w or h is equal to 0 or not a positive integer, create an empty object
+        return {};
+      }
+      // Initialize instance attributes width and height
+      this.width = w;
+      this.height = h;
     }
-} 
+  }
+  
 
 module.exports = Rectangle
