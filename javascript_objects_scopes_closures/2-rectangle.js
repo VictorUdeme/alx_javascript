745d0071315ebfa78206038563a36ea95849a860
Snazzydo <snazzydo9@gmail.com>
2023-09-07 20:33:37 +0100

    Task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,8 @@
 class Rectangle {
     constructor(w, h) {
       if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-        // If w or h is equal to 0 or not a positive integer, create an empty object
-        return {};
+        // If w or h is equal to 0 or not a positive integer, throw an error
+        throw new Error('Invalid input. Width and height must be positive integers.');
       }
       // Initialize instance attributes width and height
       this.width = w;
