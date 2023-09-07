ccf3ada5b3d7067c7c426620e0c270c4470c5897
Snazzydo <snazzydo9@gmail.com>
2023-09-07 20:49:57 +0100

    Task 5

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,23 +1,13 @@
 #!/usr/bin/node
 
-class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-        // If w or h is equal to 0 or not a positive integer, create an empty object
-        return {};
-      }
-      // Initialize instance attributes width and height
-      this.width = w;
-      this.height = h;
-    }
+const Rectangle = require('./Rectangle'); // Imports the Rectangle class from Rectangle.js
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
   }
-  
-  class Square extends Rectangle {
-    constructor(size) {
-      // Call the constructor of the parent class (Rectangle) using super()
-      super(size, size);
-    }
-  }
-  
-module.exports = Square
+}
+
+module.exports = Square; // Exports the Square class
+
   
