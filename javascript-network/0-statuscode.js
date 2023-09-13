727d1fc2d26d4cdc43869eda6e7aa81464d7279f
Snazzydo <snazzydo9@gmail.com>
2023-09-13 20:35:22 +0100

    Task 0

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url ='';
 
-request.get(url, function(error, response, body) {
-    if (response.statusCode===200) {
-        console.log(`code: ${response.statusCode}`);
-    } else {
-        console.error('Error: ', error);
+request.get('https://intranet.alxswe.com', function (error, response, body) {
+    if (error) {
+        console.log('Error: ', error);
+        return;
     }
-})
+    console.log('code:', response.statusCode);
+});
