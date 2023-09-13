09ffa2e16d09a5fbcc9560ccfa6844191fb0cd3d
Snazzydo <snazzydo9@gmail.com>
2023-09-13 22:35:49 +0100

    Task 2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -4,18 +4,28 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-request({ url: apiUrl, json: true }, (error, response, body) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    let count = 0;
-    body.results.forEach(film => {
-        if(film.characters.includes(`http://swapi.dev/api/people/${characterId}/`)) {
-            count++;
+function fetchFilms(url, count = 0) {
+    request({ url: url, json: true }, (error, response, body) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        body.results.forEach(film => {
+            if (film.characters.includes(`http://swapi.dev/api/people/${characterId}/`)) {
+                count++;
+            }
+        });
+
+        if (body.next) {
+            fetchFilms(body.next, count);
+        } else {
+            console.log(count);
         }
     });
-    console.log(count);
-});
+}
+
+fetchFilms(apiUrl);
+
 
 
