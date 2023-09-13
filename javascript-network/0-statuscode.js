23e64f2891342077ef6d07f488fa0f864ec7f15f
Snazzydo <snazzydo9@gmail.com>
2023-09-13 20:58:59 +0100

    Task 0

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,20 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Enter the URL for the GET request: ', function(url) {
-  rl.close();
-  
-  request.get(url, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log(`code: ${response.statusCode}`);
+const url ='https://intranet.alxswe.com'
+request.get(url, function(error, response, body) {
+    if (response.statusCode===200) {
+        console.log(`code: ${response.statusCode}`);
     } else {
-      console.error('Error:', error);
+        throw new Error('Error: ', error)
     }
-  });
-});
+})
