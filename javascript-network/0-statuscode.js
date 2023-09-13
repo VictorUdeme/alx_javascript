3fce095181531379fec490069127e7b8349a2e10
Snazzydo <snazzydo9@gmail.com>
2023-09-13 20:45:38 +0100

    Task 0

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 
-request.get('https://intranet.alxswe.com', function (error, response, body) {
+request.get('http://example.com/nonexistent', function (error, response, body) {
     if (error) {
         console.log('Error: ', error);
         return;
     }
     console.log('code:', response.statusCode);
 });
+
