d327314cbaf6f3ea589b0a6e9fb997a6b0a36426
Snazzydo <snazzydo9@gmail.com>
2023-09-14 12:58:43 +0100

    Task 3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,35 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
 
-const [url, outputFilePath] = process.argv.slice(2);
+const url = process.argv[2];
+const file_path = process.argv[3];
 
-if (!url || !outputFilePath) {
-    console.error('Usage: node fetch_and_store.js <URL> <output_file>');
-    process.exit(1);
-}
-
-request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        process.exit(1);
-    }
-
-    fs.writeFile(outputFilePath, body, 'utf-8', (err) => {
-        if (err) {
-            console.error('Error writing to file:', err);
-            process.exit(1);
-        }
-
-        // Print only the webpage content without additional messages
-        console.log(body);
-
-        // Check if stderr is empty
-        if (process.stderr._writableState.length === 0) {
-            console.log('[stderr]: [Anything]');
-        } else {
-            console.error('[stderr]:', process.stderr.read().toString().trim());
-        }
+request.get(url, { encoding: 'utf-8' }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(file_path, body, { encoding: 'utf-8' }, (error) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(response.body);
+      }
     });
+  }
 });
