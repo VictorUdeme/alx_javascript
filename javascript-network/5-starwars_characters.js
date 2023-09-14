c5855479c241fd13eb67dab98ffc96714b407f4d
Snazzydo <snazzydo9@gmail.com>
2023-09-14 13:46:44 +0100

    Task 5

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,26 +1,25 @@
 #!/usr/bin/node
 
-function printMovieCharacters(movieId) {
-    const url = `https://swapi.dev/api/films/${movieId}/`;
-  
-    request(url, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-      } else {
-        const movie = JSON.parse(body);
-        console.log(`Characters of ${movie.title}:`);
-        movie.characters.forEach((characterUrl) => {
-          request(characterUrl, (error, response, body) => {
-            if (error) {
-              console.error('Error:', error);
-            } else {
-              const character = JSON.parse(body);
-              console.log(character.name);
-            }
-          });
-        });
-      }
-    });
-  }
+const request = require('request');
 
+// Get the movie ID from the user
+const movieId = process.argv[2];
+
+// Make a GET request to the Star Wars API to retrieve the movie data
+request(`https://swapi.dev/api/films/${movieId}`, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    // Parse the JSON response
+    const movie = JSON.parse(body);
+
+    // Get the characters from the movie
+    const characters = movie.characters;
+
+    // Iterate over the characters and print their names
+    for (const character of characters) {
+      console.log(character.name);
+    }
+  }
+});
 
