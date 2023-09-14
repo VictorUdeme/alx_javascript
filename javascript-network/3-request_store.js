3d3b8624a35c080662b1d6add54bf513e4b3e7a3
Snazzydo <snazzydo9@gmail.com>
2023-09-14 12:53:33 +0100

    task 3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -21,6 +21,7 @@ request(url, (error, response, body) => {
             console.error('Error writing to file:', err);
             process.exit(1);
         }
-        console.log(`Webpage content has been saved to ${outputFilePath}`);
+
+        console.log(body); // Print the webpage content
     });
 });
