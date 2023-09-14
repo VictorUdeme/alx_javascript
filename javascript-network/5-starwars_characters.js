f146eab6482dc574705432e8e2b013d1b59bc921
Snazzydo <snazzydo9@gmail.com>
2023-09-14 14:05:44 +0100

    Task 5

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,46 +1,28 @@
+#!/usr/bin/node
+
 const request = require('request');
 
-function getMovieCharacters(movieId) {
-  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+const movieId = process.argv[2];
 
-  request(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(`Error fetching movie data: ${error}`);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-      return;
-    }
-
-    const movieData = JSON.parse(body);
-    const characters = movieData.characters;
-
-    // Print each character name
-    characters.forEach(async (characterUrl) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error(`Error fetching character data: ${charError}`);
-          return;
-        }
-
-        if (charResponse.statusCode !== 200) {
-          console.error(`Error: ${charResponse.statusCode} - ${charResponse.statusMessage}`);
-          return;
-        }
-
-        const characterData = JSON.parse(charBody);
-        console.log(characterData.name);
-      });
-    });
-  });
-}
-
-const movieId = process.argv[2]; // Get the movie ID from the command line argument
 if (!movieId) {
   console.error('Please provide a movie ID as an argument.');
   process.exit(1);
 }
 
-getMovieCharacters(movieId);
+request.get(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    console.error('Error fetching movie data.');
+    process.exit(1);
+  }
+
+  const movieData = JSON.parse(body);
+
+  movieData.characters.forEach((characterUrl) => {
+    request.get(characterUrl, (charError, charResponse, charBody) => {
+      if (!charError && charResponse.statusCode === 200) {
+        const characterData = JSON.parse(charBody);
+        console.log(characterData.name);
+      }
+    });
+  });
+});
