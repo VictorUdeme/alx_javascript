06cff3dbe277cab9c68ae031bd6bc5ffee8e1666
Snazzydo <snazzydo9@gmail.com>
2023-09-14 13:34:01 +0100

    Task 4

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -5,16 +5,32 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, function(error, response, body){
-    const users = JSON.parse(body);
-    const task_completed = {};
-    users.foreach((user) => {
-        if (user.completed){
-            task_completed[user.userId];
-            task_completed[user.userId]++;
+    if (error) {
+        console.error("Error:", error);
+        return;
+    }
 
-        } else {
-            task_completed[user.userId] = 1;
-        }
-    });
-    console.log(task_completed);
+    if (response.statusCode !== 200) {
+        console.error("HTTP Status Code:", response.statusCode);
+        return;
+    }
+
+    try {
+        const users = JSON.parse(body);
+        const taskCompleted = {}; // Changed the variable name to follow JavaScript naming conventions
+
+        users.forEach((user) => { // Corrected the typo 'foreach' to 'forEach'
+            if (user.completed) {
+                if (taskCompleted[user.userId]) { // Check if the property exists in the object
+                    taskCompleted[user.userId]++;
+                } else {
+                    taskCompleted[user.userId] = 1;
+                }
+            }
+        });
+
+        console.log(taskCompleted);
+    } catch (err) {
+        console.error("Error parsing JSON:", err);
+    }
 });
