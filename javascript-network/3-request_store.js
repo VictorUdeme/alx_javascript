5bf8e3f7229a731b88c4e966a813157e723a289a
Snazzydo <snazzydo9@gmail.com>
2023-09-14 12:26:10 +0100

    Task 3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,29 +1,23 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
 
 const url = process.argv[2];
-const filePath = process.argv[3];
+const file_path = process.argv[3];
 
-// Making an HTTP GET request to the specified URL
-request.get(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        return;
-    }
-
-    if (response.statusCode !== 200) {
-        console.error('Received non-200 status code:', response.statusCode);
-        return;
-    }
-
-    // Writing the response body to the specified file with UTF-8 encoding
-    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-        if (writeError) {
-            console.error('Error writing to file:', writeError);
-        } else {
-            console.log(`Saved response to ${filePath}`);
-        }
+request(url, function(error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  else {
+    fs.writeFile(file_path, body, 'utf-8', function(error) {
+      if (error) {
+        console.error(error);
+      }
+      else {
+        console.log(`Webpage content saved to ${file_path}`);
+      }
     });
+  }
 });
