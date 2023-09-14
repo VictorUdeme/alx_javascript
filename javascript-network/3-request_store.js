073a80fcfd0dfc23e61ddafaa1c26a0c81e935cd
Snazzydo <snazzydo9@gmail.com>
2023-09-14 12:50:58 +0100

    Task 3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,36 +1,26 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 
-const url = process.argv[2];
-const file_path = process.argv[3];
+const [url, outputFilePath] = process.argv.slice(2);
 
-request.get({ url, encoding: null }, function(error, response, body) {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode === 200) {
-    // Decode the binary body content as UTF-8
-    const decodedContent = body.toString('utf-8');
-    
-    fs.writeFile(file_path, decodedContent, 'utf-8', function(error) {
-      if (error) {
-        console.error(error);
-      } else {
-        // Read the saved file and print its content
-        fs.readFile(file_path, 'utf-8', function(error, data) {
-          if (error) {
-            console.error(error);
-          } else {
-            console.log(data);
-          }
-        });
-      }
+if (!url || !outputFilePath) {
+    console.error('Usage: node fetch_and_store.js <URL> <output_file>');
+    process.exit(1);
+}
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+
+    fs.writeFile(outputFilePath, body, 'utf-8', (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+            process.exit(1);
+        }
+        console.log(`Webpage content has been saved to ${outputFilePath}`);
     });
-  } else {
-    console.error(`Request to ${url} failed with status code: ${response.statusCode}`);
-  }
 });
-
-
-
