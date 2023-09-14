d3a2ae13a34791c515a700a2ebace3c9248b1fc8
Snazzydo <snazzydo9@gmail.com>
2023-09-14 13:23:31 +0100

    task 4

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -13,8 +13,8 @@ request(url, function (error, response, body) {
   // Parse the JSON response into a list of tasks
   const tasks = JSON.parse(body);
 
-  // Create a map to store the number of completed tasks for each user
-  const completedTasksByUser = new Map();
+  // Create an object to store the number of completed tasks for each user
+  const completedTasksByUser = {};
 
   // Loop through each task
   tasks.forEach(function (task) {
@@ -23,28 +23,14 @@ request(url, function (error, response, body) {
       // Get the user ID for the task
       const userId = task.userId;
       // Increment the number of completed tasks for the user
-      completedTasksByUser.set(userId, (completedTasksByUser.get(userId) || 0) + 1);
+      if (!completedTasksByUser[userId]) {
+        completedTasksByUser[userId] = 1;
+      } else {
+        completedTasksByUser[userId]++;
+      }
     }
   });
-
-  // Filter the results to include only users 1 and 2
-  const filteredResults = new Map();
-  completedTasksByUser.forEach(function (numCompletedTasks, userId) {
-    if (userId === 1 || userId === 2) {
-      filteredResults.set(userId, numCompletedTasks);
-    }
-  });
-
-  // Check if users 1 and 2 exist in the filtered results, and if not, set their counts to 0
-  if (!filteredResults.has(1)) {
-    filteredResults.set(1, 0);
-  }
-  if (!filteredResults.has(2)) {
-    filteredResults.set(2, 0);
-  }
 
   // Print the number of completed tasks for users 1 and 2
-  filteredResults.forEach(function (numCompletedTasks, userId) {
-    console.log(`User ${userId} completed ${numCompletedTasks} task(s).`);
-  });
+  console.log(completedTasksByUser);
 });
